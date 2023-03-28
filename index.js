ff49da2c47d4b01d6ee29ac7e1a2f691f21951f1
Ömer <omergungorco@gmail.com>
2023-03-28 23:46:28 +0300

    Basic functions added

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const serve = require('koa-static');
+const path = require('path');
+
+
 const app = new Koa();
+const router = new Router();
+
+// statik dosyalar için bir klasör tanımlayın
+const staticFiles = serve(path.join(__dirname, 'public'));
+
+
+
 
 // response
 app.use(ctx => {
-    ctx.body = 'Hello Koa';
+    console.log(ctx.url);
+    if (ctx.URL.pathname === '/index') {
+        ctx.body = 'Index Page';
+    } else if (ctx.url === '/iletisim') {
+        ctx.body = 'iletisim Index';
+    } else if (ctx.url === '/hakkimda') {
+        ctx.body = 'hakkimda Index';
+    } else {
+        ctx.body = '404 Page not found';
+    }
 });
 
 app.listen(3000);
